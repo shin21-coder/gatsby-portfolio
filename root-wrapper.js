f10bd9fdb3d51c5f14eb0adf79a25b22e5e59879
shin21-coder <shingohorikawa3@gmail.com>
2020-09-01 16:59:32 +0900

    fontawesome,おわりました！

---
+++ root-wrapper.js
@@ -0,0 +1,41 @@
+// import React from 'react'
+// import { createGlobalStyle } from 'styled-components'
+// // import {GatsbyProvider} from './src/context/context'
+
+// const GlobalStyle = createGlobalStyle`
+// /*
+// =============== 
+// Variables
+// ===============
+// */
+
+// /*
+// =============== 
+// Global Styles
+// ===============
+// */
+
+// *,
+// ::after,
+// ::before {
+//   margin: 0;
+//   padding: 0;
+//   box-sizing: border-box;
+// }
+// body {
+//   line-height: 1.5;
+//   font-size: 0.875rem;
+// }
+
+// `
+
+// export const wrapRootElement = ({ element }) => {
+//   return <>
+//     <GlobalStyle>
+//       {element}
+//     </GlobalStyle>
+
+
+//     {/* //ここにもいましたGatsbyProvider */}
+//   </>//ここで設定したものはページ全てに影響する
+// }
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -10,7 +10,10 @@ import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
 import tileData from '../images/tileData.js';
-import Image from '../../static/background.jpg'
+import Image from '../../static/background.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -63,11 +66,15 @@ const useStyles = makeStyles((theme) => ({
   },
   subtitle: {
     fontSize:'2rem'
+  },
+  awesome: {
+    fontSize:'5rem'
   }
 }));
 
 export default function AutoGrid() {
   const classes = useStyles();
+  library.add(fab) // FontAwesomeのライブラリ読み込み
 
   return (
     <Layout>
@@ -84,12 +91,13 @@ export default function AutoGrid() {
           </Grid>
         </Grid>
         <Grid container spacing={3}>
-          {/* <Grid item xs>
+          <Grid item xs>
             <Paper className={classes.paper}>xs</Paper>
           </Grid>
           <Grid item xs={10}>
-            <Paper className={classes.paper}>xs=10</Paper>
-          </Grid> */}
+            <Paper className={classes.paper}></Paper>
+
+          </Grid>
           <Grid item xs>
             <Paper className={classes.paper}>
               <div className={classes.rootgrid}>
@@ -97,21 +105,20 @@ export default function AutoGrid() {
                   <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
                     <ListSubheader component="h3" className={classes.subtitle}>Skill Lists</ListSubheader>
                   </GridListTile>
-                  {tileData.map((tile) => (
-                    <GridListTile key={tile.img}>
-                      <img src={tile.img} alt={tile.title} />
-                      <GridListTileBar
-                        title={tile.title}
-                        subtitle={<span>by: {tile.author}</span>}
+               
+                    <GridListTile>
+                      {/* <img src={tile.img} alt={tile.title} /> */}
+                    <FontAwesomeIcon icon={['fab','html5']} className={classes.awesome} />
+                    
 
-                        // actionIcon={
-                        //   <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
-                        //     <InfoIcon />
-                        //   </IconButton>
-                        // }iconはいらない
+                      <GridListTileBar
+                        title='HTML'
+                        subtitle={<span>html</span>}
+
+
                       />
                     </GridListTile>
-                  ))}
+                 
                 </GridList>
               </div>
 
